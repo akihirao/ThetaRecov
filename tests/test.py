dbbf002ece8f3a8fe108c88665e5c7722922d0ea
akihirao <akihirao@gmai.com>
2025-02-25 16:28:48 +0900

    revised

--- tests/test.py
+++ tests/test.py
@@ -5,13 +5,14 @@ import pandas as pd
 
 from cyvcf2 import VCF
 
-from ThetaRecov import ThetaRecov
-from ThetaRecov import calc
+import ThetaRecov
+from ThetaRecov.core import calc_tajimaD_overall
+from ThetaRecov.core import calc_tajimaD_windows
 
 # Computation of Tajima's D across overall sites to write a csv file.
-#tajimaD_overall(vcf_path, output_path)
-calc.tajimaD_overall("test.vcf.gz","TajimaD_overall.csv")
+#tajimaD_overall input_vcf output_csv
+ThetaRecov.cor.tajimaD_overall test.vcf.gz test_TajimaD_overall.csv
 
 # Computation of Tajima's D in sliding windows to write a csv file.
-#tajimaD_windows(vcf_path, window size, output_path)
-calc.tajimaD_windows("test.vcf.gz",1000,"TajimaD_windows.csv")
+#tajimaD_windows input_vcf window size output_csv
+ThetaRecov.cor.tajimaD_windows test.vcf.gz 1000 test_TajimaD_windows.csv
