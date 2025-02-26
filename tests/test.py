1929aefa03c9efd68fd4f97862d8020015ceb868
akihirao <akihirao@gmail.com>
2025-02-26 22:19:50 +0900

    revised tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-import sys
+import unittest
 
 import numpy as np
 import pandas as pd
@@ -9,10 +9,18 @@ import ThetaRecov
 from ThetaRecov.core import calc_tajimaD_overall
 from ThetaRecov.core import calc_tajimaD_windows
 
-# Computation of Tajima's D across overall sites to write a csv file.
-#tajimaD_overall input_vcf output_csv
-ThetaRecov.cor.tajimaD_overall test.vcf.gz test_TajimaD_overall.csv
 
-# Computation of Tajima's D in sliding windows to write a csv file.
-#tajimaD_windows input_vcf window size output_csv
-ThetaRecov.cor.tajimaD_windows test.vcf.gz 1000 test_TajimaD_windows.csv
+class TestTajimadOverall(unittest.TestCase):
+	"""test class of calc_tajimaD_overall
+	"""
+
+	def test_tajimaD_overall(self):
+		"""test method for calc_tajimaD_overall
+		"""
+		actual = ThetaRecov.core.calc_tajimaD_overall("test.vcf.gz", output_csv="test_tajimaD_overall.csv")
+		expected = pd.read_csv("tajimaD_overall.csv")
+
+        assert_frame_equal(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
