7ff08571b14cacc1f17a2a482ecc04f7b7ed6a6c
akihirao <akihirao@gmail.com>
2025-02-26 23:11:38 +0900

    update

--- tests/test_tajimaD.py
+++ tests/test_tajimaD.py
@@ -1,4 +1,5 @@
 import unittest
+import filecmp
 
 import numpy as np
 import pandas as pd
@@ -12,16 +13,27 @@ from ThetaRecov.core import calc_tajimaD_overall
 from ThetaRecov.core import calc_tajimaD_windows
 
 
-class TestTajimadOverall(unittest.TestCase):
-	"""test class of calc_tajimaD_overall
+class Test_calc_tajimaD(unittest.TestCase):
+	"""
+	test class of calc_tajimaD_overall/calc_tajimaD_windows
 	"""
 
 	def test_tajimaD_overall(self):
-		"""test method for calc_tajimaD_overall
+		"""
+		test method for calc_tajimaD_overall
 		"""
 		actual = ThetaRecov.core.calc_tajimaD_overall("test.vcf.gz", output_csv="test_tajimaD_overall.csv")
 		expected = pd.read_csv("tajimaD_overall.csv")
 		assert_frame_equal(actual, expected)
+
+	def test_tajimaD_windows(self):
+		"""
+		test method for calc_tajimaD_windows
+		"""
+		actual = ThetaRecov.core.calc_tajimaD_windows("test.vcf.gz", windows_size=1000,output_csv="test_tajimaD_windows.csv")
+		#expected = pd.read_csv("tajimaD_windows.csv")
+	    #self.assertTrue(filecmp.cmp("tajimaD_windows.csv","test_tajimaD_windows.csv",shallow=False))
+
 
 if __name__ == "__main__":
     unittest.main()
