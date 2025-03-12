73729369a35ce00467b54e750f5f0f9b2ecc8cdc
akihirao <akihirao@gmail.com>
2025-03-12 13:08:27 +0900

    update

--- ThetaRecov/pi_inbreed_multi.py
+++ ThetaRecov/pi_inbreed_multi.py
@@ -30,13 +30,16 @@ def main():
     
     required.add_argument('--input_vcf', '-in', type=str, nargs = '?', help = 'Input a VCF/VCF.gz file', required = True)
     optional.add_argument('--output_csv', '-out', type=str, nargs = '?', help = 'Output a csv file (default: inbreed.csv)', default = "inbreed.csv")
-    
+    optional.add_argument('--threads', '-t', type=int, nargs = '?', help = 'Number of threads (default: 2)', default = 2)
+   
     args = parser.parse_args()
 
     results = []
 
     IN_VCF = args.input_vcf
     vcf_reader = VCF(IN_VCF)
+
+    num_threads = args.threads
 
     L = vcf_reader.seqlens[0] #length of sequences
     samples = vcf_reader.samples #list of samples
@@ -50,18 +53,18 @@ def main():
     result_within = []
 
     for i in range(num_samples):
-        print(f"Processing individual {i}")# for debug
+        #print(f"Processing individual {i}")# for debug
         result_within.append(ThetaRecov.core.calc_pi_within_elements_indiv_i(IN_VCF, i))
     diff_count_within = np.array(result_within).sum(axis=0)
 
-    with Pool(4) as pool:
-        result_among =  pool.map(partial(ThetaRecov.core.calc_pi_among_elements_indiv_ij, IN_VCF), pairs[:20])
-        #result_among =  pool.map(partial(ThetaRecov.core.calc_pi_among_elements_indiv_ij, IN_VCF), pairs)
+    with Pool(num_threads) as pool:
+        #result_among =  pool.map(partial(ThetaRecov.core.calc_pi_among_elements_indiv_ij, IN_VCF), pairs[:20])
+        result_among =  pool.map(partial(ThetaRecov.core.calc_pi_among_elements_indiv_ij, IN_VCF), pairs)
     diff_count_among = np.array(result_among).sum(axis=0)
 
-    print(f"diff_count_within: {diff_count_within}") #for debug
+    #print(f"diff_count_within: {diff_count_within}") #for debug
     print(f"diff_count_among: {diff_count_among}") #for debug
-    
+
     pi_overall = (diff_count_within[0] + diff_count_among[0])/(diff_count_within[1] + diff_count_among[1])
     pi_within = diff_count_within[0]/diff_count_within[1]
     pi_among = diff_count_among[0]/diff_count_among[1]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def requirements_from_file(file_name):
 
 setup(
 	names="ThetaRecov",
-	version="0.0.31",
+	version="0.0.32",
 	packages=find_packages(),
 	install_requires=requirements_from_file('requirements.txt'),
 	entry_points={
