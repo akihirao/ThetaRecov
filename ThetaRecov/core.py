78394ee003de0274da27c08530f5c9dd8a9f6d62
akihirao <akihirao@gmail.com>
2025-03-13 16:32:53 +0900

    revised

--- ThetaRecov/core.py
+++ ThetaRecov/core.py
@@ -420,7 +420,7 @@ def diff_count_among_ne(gt_matrix):
     valid_row1 = np.where(valid_mask, row1, 0)
     valid_row2 = np.where(valid_mask, row2, 0)
 
-    diff_among = ne.evaluate("sum(abs(valid_row1-valid_row2)")
+    diff_among = ne.evaluate("sum(abs(valid_row1 - valid_row2))")
     count_among = ne.evaluate("sum(valid_mask)")
 
     #abs_diff = np.abs(row1 - row2)
