12b8de6a814d678759efb721d138b3f3497b5db3
akihirao <akihirao@gmail.com>
2025-03-13 14:14:36 +0900

    exe ok

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def requirements_from_file(file_name):
 
 setup(
 	names="ThetaRecov",
-	version="0.0.84",
+	version="0.0.85",
 	packages=find_packages(),
 	install_requires=requirements_from_file('requirements.txt'),
 	entry_points={
